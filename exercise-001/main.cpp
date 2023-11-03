e879555264d07ff2988946b1bdb37d9d64a056e4
Erik Schneider <72032312+Wangchukie@users.noreply.github.com>
2023-11-03 15:12:46 +0100

    whoop whoop

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -7,16 +7,23 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
-int foo;
+int bss;
+int data = 4711;
+int rodata;
 
 auto main(int argc, char **argv) -> int
 {
+
+    fmt::print("Value of variable bss {}address of varaible bss{}\n", bss, fmt::ptr(&bss));
+    fmt::print("Value of variable data {}address of varaible data{}\n", data, fmt::ptr(&data));
+    fmt::print("Value of variable rodata {}address of varaible rodata{}\n", rodata, fmt::ptr(&rodata));
+
    
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", "Timo");
+    fmt::print("\nHello, {}!\n", "Timo");
     return 0; /* exit gracefully*/
 }
