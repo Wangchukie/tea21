d4312788ccccae39bcaca377c5a176046bfe4032
Erik Schneider <72032312+Wangchukie@users.noreply.github.com>
2023-11-10 08:57:09 +0100

    .

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -11,13 +11,18 @@ int bss;
 int data = 4711;
 const int rodata = 42;
 
+int function(int value){
+    int data = 4711;
+    return function(data + value);
+}
+
 auto main(int argc, char **argv) -> int
 {
     int stack = 3;
-    fmt::print("Value of variable bss:    {} address of variable bss:    {}\n", bss, fmt::ptr(&bss));
-    fmt::print("Value of variable data:   {} address of variable data:   {}\n", data, fmt::ptr(&data));
-    fmt::print("Value of variable rodata: {} address of variable rodata: {}\n", rodata, fmt::ptr(&rodata));
-    fmt::print("Value of variable stack:  {} address of variable stack:  {}\n", stack, fmt::ptr(&stack));
+    fmt::print("Value of variable bss:    {} \taddress of variable bss:    {}\n", bss, fmt::ptr(&bss));
+    fmt::print("Value of variable data:   {} \taddress of variable data:   {}\n", data, fmt::ptr(&data));
+    fmt::print("Value of variable rodata: {} \taddress of variable rodata: {}\n", rodata, fmt::ptr(&rodata));
+    fmt::print("Value of variable stack:  {} \taddress of variable stack:  {}\n", stack, fmt::ptr(&stack));
 
    
     /**
@@ -25,6 +30,7 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
+    //function(33);
     fmt::print("\nHello, {}!\n", "Timo");
     return 0; /* exit gracefully*/
 }
